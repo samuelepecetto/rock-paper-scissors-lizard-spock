b0f2ade946feb7dc73a12802db20c6f891d86571
Samuele Pecetto <samuele.pecetto@newday.co.uk>
2023-09-15 12:20:55 +0100

    fix tests

--- RockPaperScissor.UnitTests/GetOutcomeTests.fs
+++ RockPaperScissor.UnitTests/GetOutcomeTests.fs
@@ -2,123 +2,167 @@ module GetOutcomeTests
 
 open RockPaperScissors.RockPaperScissors
 open Xunit
-open RockPaperScissors
 
-type GetOutcomeTests() =
-    // --------- Player1Wins ---------
-    
-    // Scissors cuts Paper
-    [<Fact>]
-    member this.``Scissors vs Paper should result in Player1Wins``() =
-        let result = getOutcome Sign.Scissors Sign.Paper
-        Assert.Equal(Outcome.Player1Wins, result)
+// --------- Win ---------
 
-    // Paper covers Rock
-    [<Fact>]
-    member this.``Paper vs Rock should result in Player1Wins``() =
-        let result = getOutcome Sign.Paper Sign.Rock
-        Assert.Equal(Outcome.Player1Wins, result)
+// Scissors cuts Paper
+[<Fact>]
+let ``Scissors vs Paper should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    // Rock crushes Lizard
-    [<Fact>]
-    member this.``Rock vs Lizard should result in Player1Wins``() =
-        let result = getOutcome Sign.Rock Sign.Lizard
-        Assert.Equal(Outcome.Player1Wins, result)
+    match scoreBoard.PlayGame Sign.Scissors Sign.Paper with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    // Lizard poisons Spock
-    [<Fact>]
-    member this.``Lizard vs Spock should result in Player1Wins``() =
-        let result = getOutcome Sign.Lizard Sign.Spock
-        Assert.Equal(Outcome.Player1Wins, result)
+// Paper covers Rock
+[<Fact>]
+let ``Paper vs Rock should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    // Spock smashes Scissors
-    [<Fact>]
-    member this.``Spock vs Scissors should result in Player1Wins``() =
-        let result = getOutcome Sign.Spock Sign.Scissors
-        Assert.Equal(Outcome.Player1Wins, result)
+    match scoreBoard.PlayGame Sign.Paper Sign.Rock with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    // Scissors decapitates Lizard
-    [<Fact>]
-    member this.``Scissors vs Lizard should result in Player1Wins``() =
-        let result = getOutcome Sign.Scissors Sign.Lizard
-        Assert.Equal(Outcome.Player1Wins, result)
+// Rock crushes Lizard
+[<Fact>]
+let ``Rock vs Lizard should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    // Lizard eats Paper
-    [<Fact>]
-    member this.``Lizard vs Paper should result in Player1Wins``() =
-        let result = getOutcome Sign.Lizard Sign.Paper
-        Assert.Equal(Outcome.Player1Wins, result)
+    match scoreBoard.PlayGame Sign.Rock Sign.Lizard with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    // Paper disproves Spock
-    [<Fact>]
-    member this.``Paper vs Spock should result in Player1Wins``() =
-        let result = getOutcome Sign.Paper Sign.Spock
-        Assert.Equal(Outcome.Player1Wins, result)
+// Lizard poisons Spock
+[<Fact>]
+let ``Lizard vs Spock should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    // Spock vaporizes Rock
-    [<Fact>]
-    member this.``Spock vs Rock should result in Player1Wins``() =
-        let result = getOutcome Sign.Spock Sign.Rock
-        Assert.Equal(Outcome.Player1Wins, result)
+    match scoreBoard.PlayGame Sign.Lizard Sign.Spock with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    // and as it always has Rock crushes Scissors
-    [<Fact>]
-    member this.``Rock vs Scissors should result in Player1Wins``() =
-        let result = getOutcome Sign.Rock Sign.Scissors
-        Assert.Equal(Outcome.Player1Wins, result)
+// Spock smashes Scissors
+[<Fact>]
+let ``Spock vs Scissors should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
+    match scoreBoard.PlayGame Sign.Spock Sign.Scissors with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
+// Scissors decapitates Lizard
+[<Fact>]
+let ``Scissors vs Lizard should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    // --------- Draws ---------
-    [<Fact>]
-    member this.``Rock vs Rock should result in Draw``() =
-        let result = getOutcome Sign.Rock Sign.Rock
-        Assert.Equal(Outcome.Draw, result)
+    match scoreBoard.PlayGame Sign.Scissors Sign.Lizard with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    [<Fact>]
-    member this.``Paper vs Paper should result in Draw``() =
-        let result = getOutcome Sign.Paper Sign.Paper
-        Assert.Equal(Outcome.Draw, result)
+// Lizard eats Paper
+[<Fact>]
+let ``Lizard vs Paper should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    [<Fact>]
-    member this.``Scissors vs Scissors should result in Draw``() =
-        let result = getOutcome Sign.Scissors Sign.Scissors
-        Assert.Equal(Outcome.Draw, result)
+    match scoreBoard.PlayGame Sign.Lizard Sign.Paper with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    [<Fact>]
-    member this.``Lizard vs Lizard should result in Draw``() =
-        let result = getOutcome Sign.Lizard Sign.Lizard
-        Assert.Equal(Outcome.Draw, result)
+// Paper disproves Spock
+[<Fact>]
+let ``Paper vs Spock should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    [<Fact>]
-    member this.``Spock vs Spock should result in Draw``() =
-        let result = getOutcome Sign.Spock Sign.Spock
-        Assert.Equal(Outcome.Draw, result)
+    match scoreBoard.PlayGame Sign.Paper Sign.Spock with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
 
-    // --------- Player2Wins ---------
+// Spock vaporizes Rock
+[<Fact>]
+let ``Spock vs Rock should result in Win`` () =
+    let scoreBoard = ScoreBoard()
 
-    [<Fact>]
-    member this.``Rock vs Paper should result in Player2Wins``() =
-        let result = getOutcome Sign.Rock Sign.Paper
-        Assert.Equal(Outcome.Player2Wins, result)
-    
-    [<Fact>]
-    member this.``Paper vs Scissors should result in Player2Wins``() =
-        let result = getOutcome Sign.Paper Sign.Scissors
-        Assert.Equal(Outcome.Player2Wins, result)
-    
-    [<Fact>]
-    member this.``Scissors vs Rock should result in Player2Wins``() =
-        let result = getOutcome Sign.Scissors Sign.Rock
-        Assert.Equal(Outcome.Player2Wins, result)
-    
-    [<Fact>]
-    member this.``Paper vs Lizard should result in Player2Wins``() =
-        let result = getOutcome Sign.Paper Sign.Lizard
-        Assert.Equal(Outcome.Player2Wins, result)
-    
-    [<Fact>]
-    member this.``Spock vs Lizard should result in Player2Wins``() =
-        let result = getOutcome Sign.Spock Sign.Lizard
-        Assert.Equal(Outcome.Player2Wins, result)
-    
+    match scoreBoard.PlayGame Sign.Spock Sign.Rock with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+// and as it always has Rock crushes Scissors
+[<Fact>]
+let ``Rock vs Scissors should result in Win`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Rock Sign.Scissors with
+    | GameResult.Win(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+// --------- Draws ---------
+[<Fact>]
+let ``Rock vs Rock should result in Draw`` () =
+    let scoreBoard = ScoreBoard()
+    let result = scoreBoard.PlayGame Sign.Rock Sign.Rock
+    Assert.Equal(GameResult.Draw, result)
+
+[<Fact>]
+let ``Paper vs Paper should result in Draw`` () =
+    let scoreBoard = ScoreBoard()
+    let result = scoreBoard.PlayGame Sign.Paper Sign.Paper
+    Assert.Equal(GameResult.Draw, result)
+
+[<Fact>]
+let ``Scissors vs Scissors should result in Draw`` () =
+    let scoreBoard = ScoreBoard()
+    let result = scoreBoard.PlayGame Sign.Scissors Sign.Scissors
+    Assert.Equal(GameResult.Draw, result)
+
+[<Fact>]
+let ``Lizard vs Lizard should result in Draw`` () =
+    let scoreBoard = ScoreBoard()
+    let result = scoreBoard.PlayGame Sign.Lizard Sign.Lizard
+    Assert.Equal(GameResult.Draw, result)
+
+[<Fact>]
+let ``Spock vs Spock should result in Draw`` () =
+    let scoreBoard = ScoreBoard()
+    let result = scoreBoard.PlayGame Sign.Spock Sign.Spock
+    Assert.Equal(GameResult.Draw, result)
+
+// --------- GameResult.Loss ---------
+[<Fact>]
+let ``Rock vs Paper should result in Loss`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Rock Sign.Paper with
+    | GameResult.Loss(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+[<Fact>]
+let ``Paper vs Scissors should result in Loss`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Paper Sign.Scissors with
+    | GameResult.Loss(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+[<Fact>]
+let ``Scissors vs Rock should result in Loss`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Scissors Sign.Rock with
+    | GameResult.Loss(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+[<Fact>]
+let ``Paper vs Lizard should result in Loss`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Paper Sign.Lizard with
+    | GameResult.Loss(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
+
+[<Fact>]
+let ``Spock vs Lizard should result in Loss`` () =
+    let scoreBoard = ScoreBoard()
+
+    match scoreBoard.PlayGame Sign.Spock Sign.Lizard with
+    | GameResult.Loss(_) -> Assert.True(true)
+    | _ -> Assert.True(false)
--- RockPaperScissors.fs
+++ RockPaperScissors.fs
@@ -1,12 +1,7 @@
 ﻿module RockPaperScissors =
     type GameResult =
-        | Win of int  // Player 1 wins with a value indicating the number of wins
+        | Win of int // Player 1 wins with a value indicating the number of wins
         | Loss of int // Player 2 wins with a value indicating the number of wins
-        | Draw
-        
-    type Outcome =
-        | Player1Wins
-        | Player2Wins
         | Draw
 
     type Sign =
@@ -15,7 +10,7 @@
         | Scissors
         | Lizard
         | Spock
-        
+
     type ScoreBoard() =
         let mutable player1Score = 0
         let mutable player2Score = 0
@@ -24,32 +19,35 @@
         member this.Player2Score = player2Score
 
         member this.PlayGame (sign1: Sign) (sign2: Sign) : GameResult =
-            match sign1, sign2 with
-            | Rock, Scissors
-            | Scissors, Lizard
-            | Lizard, Spock
-            | Spock, Scissors
-            | Scissors, Paper
-            | Paper, Rock
-            | Rock, Lizard
-            | Lizard, Paper
-            | Paper, Spock
-            | Spock, Rock ->
-                player1Score <- player1Score + 1
-                GameResult.Win(player1Score)
+            let gameResult =
+                match sign1, sign2 with
+                | Rock, Scissors
+                | Scissors, Lizard
+                | Lizard, Spock
+                | Spock, Scissors
+                | Scissors, Paper
+                | Paper, Rock
+                | Rock, Lizard
+                | Lizard, Paper
+                | Paper, Spock
+                | Spock, Rock ->
+                    player1Score <- player1Score + 1
+                    GameResult.Win(player1Score)
 
 
-            | Scissors, Rock
-            | Lizard, Scissors
-            | Spock, Lizard
-            | Scissors, Spock
-            | Paper, Scissors
-            | Rock, Paper
-            | Lizard, Rock
-            | Paper, Lizard
-            | Spock, Paper
-            | Rock, Spock -> 
-                player2Score <- player2Score + 1
-                GameResult.Loss(player2Score)
+                | Scissors, Rock
+                | Lizard, Scissors
+                | Spock, Lizard
+                | Scissors, Spock
+                | Paper, Scissors
+                | Rock, Paper
+                | Lizard, Rock
+                | Paper, Lizard
+                | Spock, Paper
+                | Rock, Spock ->
+                    player2Score <- player2Score + 1
+                    GameResult.Loss(player2Score)
 
-            | _ -> GameResult.Draw
+                | _ -> GameResult.Draw
+
+            gameResult
