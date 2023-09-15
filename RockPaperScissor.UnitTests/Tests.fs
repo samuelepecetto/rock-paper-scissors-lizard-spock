610a530dced5beca87d45004a388f1da83dd4e17
Samuele Pecetto <samuele.pecetto@newday.co.uk>
2023-09-15 10:48:41 +0100

    Basic Algo implementation

---
+++ RockPaperScissor.UnitTests/Tests.fs
@@ -0,0 +1,124 @@
+module Tests
+
+open RockPaperScissors.RockPaperScissors
+open Xunit
+open RockPaperScissors
+
+type GetOutcomeTests() =
+    // --------- Player1Wins ---------
+    
+    // Scissors cuts Paper
+    [<Fact>]
+    member this.``Scissors vs Paper should result in Player1Wins``() =
+        let result = getOutcome Sign.Scissors Sign.Paper
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Paper covers Rock
+    [<Fact>]
+    member this.``Paper vs Rock should result in Player1Wins``() =
+        let result = getOutcome Sign.Paper Sign.Rock
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Rock crushes Lizard
+    [<Fact>]
+    member this.``Rock vs Lizard should result in Player1Wins``() =
+        let result = getOutcome Sign.Rock Sign.Lizard
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Lizard poisons Spock
+    [<Fact>]
+    member this.``Lizard vs Spock should result in Player1Wins``() =
+        let result = getOutcome Sign.Lizard Sign.Spock
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Spock smashes Scissors
+    [<Fact>]
+    member this.``Spock vs Scissors should result in Player1Wins``() =
+        let result = getOutcome Sign.Spock Sign.Scissors
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Scissors decapitates Lizard
+    [<Fact>]
+    member this.``Scissors vs Lizard should result in Player1Wins``() =
+        let result = getOutcome Sign.Scissors Sign.Lizard
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Lizard eats Paper
+    [<Fact>]
+    member this.``Lizard vs Paper should result in Player1Wins``() =
+        let result = getOutcome Sign.Lizard Sign.Paper
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Paper disproves Spock
+    [<Fact>]
+    member this.``Paper vs Spock should result in Player1Wins``() =
+        let result = getOutcome Sign.Paper Sign.Spock
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // Spock vaporizes Rock
+    [<Fact>]
+    member this.``Spock vs Rock should result in Player1Wins``() =
+        let result = getOutcome Sign.Spock Sign.Rock
+        Assert.Equal(Outcome.Player1Wins, result)
+
+    // and as it always has Rock crushes Scissors
+    [<Fact>]
+    member this.``Rock vs Scissors should result in Player1Wins``() =
+        let result = getOutcome Sign.Rock Sign.Scissors
+        Assert.Equal(Outcome.Player1Wins, result)
+
+
+
+    // --------- Draws ---------
+    [<Fact>]
+    member this.``Rock vs Rock should result in Draw``() =
+        let result = getOutcome Sign.Rock Sign.Rock
+        Assert.Equal(Outcome.Draw, result)
+
+    [<Fact>]
+    member this.``Paper vs Paper should result in Draw``() =
+        let result = getOutcome Sign.Paper Sign.Paper
+        Assert.Equal(Outcome.Draw, result)
+
+    [<Fact>]
+    member this.``Scissors vs Scissors should result in Draw``() =
+        let result = getOutcome Sign.Scissors Sign.Scissors
+        Assert.Equal(Outcome.Draw, result)
+
+    [<Fact>]
+    member this.``Lizard vs Lizard should result in Draw``() =
+        let result = getOutcome Sign.Lizard Sign.Lizard
+        Assert.Equal(Outcome.Draw, result)
+
+    [<Fact>]
+    member this.``Spock vs Spock should result in Draw``() =
+        let result = getOutcome Sign.Spock Sign.Spock
+        Assert.Equal(Outcome.Draw, result)
+
+    // --------- Player2Wins ---------
+
+    [<Fact>]
+    member this.``Rock vs Paper should result in Player2Wins``() =
+        let result = getOutcome Sign.Rock Sign.Paper
+        Assert.Equal(Outcome.Player2Wins, result)
+    
+    [<Fact>]
+    member this.``Paper vs Scissors should result in Player2Wins``() =
+        let result = getOutcome Sign.Paper Sign.Scissors
+        Assert.Equal(Outcome.Player2Wins, result)
+    
+    [<Fact>]
+    member this.``Scissors vs Rock should result in Player2Wins``() =
+        let result = getOutcome Sign.Scissors Sign.Rock
+        Assert.Equal(Outcome.Player2Wins, result)
+    
+    [<Fact>]
+    member this.``Paper vs Lizard should result in Player2Wins``() =
+        let result = getOutcome Sign.Paper Sign.Lizard
+        Assert.Equal(Outcome.Player2Wins, result)
+    
+    [<Fact>]
+    member this.``Spock vs Lizard should result in Player2Wins``() =
+        let result = getOutcome Sign.Spock Sign.Lizard
+        Assert.Equal(Outcome.Player2Wins, result)
+    
---
+++ RockPaperScissors.fs
@@ -0,0 +1,39 @@
+﻿module RockPaperScissors =
+
+    type Outcome =
+        | Player1Wins
+        | Player2Wins
+        | Draw
+
+    type Sign =
+        | Rock
+        | Paper
+        | Scissors
+        | Lizard
+        | Spock
+        
+    let getOutcome (sign1: Sign) (sign2: Sign) : Outcome =
+        match sign1, sign2 with
+        | Rock, Scissors
+        | Scissors, Lizard
+        | Lizard, Spock
+        | Spock, Scissors
+        | Scissors, Paper
+        | Paper, Rock
+        | Rock, Lizard
+        | Lizard, Paper
+        | Paper, Spock
+        | Spock, Rock -> Outcome.Player1Wins
+
+        | Scissors, Rock
+        | Lizard, Scissors
+        | Spock, Lizard
+        | Scissors, Spock
+        | Paper, Scissors
+        | Rock, Paper
+        | Lizard, Rock
+        | Paper, Lizard
+        | Spock, Paper
+        | Rock, Spock -> Outcome.Player2Wins
+
+        | _ -> Outcome.Draw
